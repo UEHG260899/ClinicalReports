866bbd7b8aa11210fcf9d300800710a961a373ed
Uriel <urielenrique99@gmail.com>
2020-11-28 16:37:57 -0600

    Se creo el layout basico de las pantallas del profesor

---
+++ app/src/main/java/com/example/clinicalreports/ui/listaRep/ListarRepFragment.java
@@ -0,0 +1,38 @@
+package com.example.clinicalreports.ui.listaRep;
+
+import androidx.lifecycle.ViewModelProvider;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.clinicalreports.R;
+
+public class ListarRepFragment extends Fragment {
+
+    private ListarRepViewModel mViewModel;
+
+    public static ListarRepFragment newInstance() {
+        return new ListarRepFragment();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_listar_rep, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = new ViewModelProvider(this).get(ListarRepViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/com/example/clinicalreports/ui/listaRep/ListarRepViewModel.java
@@ -0,0 +1,7 @@
+package com.example.clinicalreports.ui.listaRep;
+
+import androidx.lifecycle.ViewModel;
+
+public class ListarRepViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
