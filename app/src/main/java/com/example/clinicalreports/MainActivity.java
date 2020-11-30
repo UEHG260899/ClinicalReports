0511551ff4cdef845c2d72bde680b0d4e60324ea
Uriel <urielenrique99@gmail.com>
2020-11-29 18:42:00 -0600

    Se implemento firebase

--- app/src/main/java/com/example/clinicalreports/MainActivity.java
+++ app/src/main/java/com/example/clinicalreports/MainActivity.java
@@ -1,28 +1,87 @@
 package com.example.clinicalreports;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
 
     Button btnIn;
+    TextView tvSignin;
+    EditText etCorreo, etPass;
+    FirebaseAuth firebaseAuth;
+
+    private FirebaseAuth.AuthStateListener authStateListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        firebaseAuth = FirebaseAuth.getInstance();
         btnIn = findViewById(R.id.btnIngresar);
+        tvSignin = findViewById(R.id.tvSignIn);
+        etCorreo = findViewById(R.id.etClaveR);
+        etPass = findViewById(R.id.etCont);
+        authStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if(user != null){
+                    Toast.makeText(MainActivity.this, "Inicio de sesión exitoso", Toast.LENGTH_SHORT).show();
+                    //Consulta para saber si es alumno o maestro
+                }
+            }
+        };
 
         btnIn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, DrawerMaestro.class);
+                String email = etCorreo.getText().toString();
+                String pass = etPass.getText().toString();
+
+                if(email.isEmpty()){
+                    etCorreo.requestFocus();
+                    etCorreo.setError("Ingrese un correo");
+                }else if(pass.isEmpty()){
+                    etPass.requestFocus();
+                    etCorreo.setError("Ingrese una contraseña");
+                }else if (email.isEmpty() && pass.isEmpty()){
+                    etCorreo.requestFocus();
+                    etPass.requestFocus();
+                    Toast.makeText(MainActivity.this, "No puede haber campos vacios", Toast.LENGTH_SHORT).show();
+                }else if(!(email.isEmpty() && pass.isEmpty())){
+                    firebaseAuth.signInWithEmailAndPassword(email, pass).addOnCompleteListener(MainActivity.this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if(!task.isSuccessful()){
+                                Toast.makeText(MainActivity.this, "Ocurrio un error", Toast.LENGTH_LONG);
+                            }
+                        }
+                    });
+                }
+            }
+        });
+
+        tvSignin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, RegistroActivity.class);
                 startActivity(intent);
+                finish();
             }
         });
     }
--- app/src/main/java/com/example/clinicalreports/RegistroActivity.java
+++ app/src/main/java/com/example/clinicalreports/RegistroActivity.java
@@ -1,14 +1,94 @@
 package com.example.clinicalreports;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TimePicker;
+import android.widget.Toast;
+
+import com.example.clinicalreports.mdbf.Profesor;
+import com.google.android.gms.common.util.JsonUtils;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class RegistroActivity extends AppCompatActivity {
+
+
+    Button btnRegistrar;
+    EditText etNombre, etEmail, etPass, etPassConf, etClave;
+    FirebaseAuth firebaseAuth;
+    FirebaseDatabase firebaseDatabase;
+    DatabaseReference databaseReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro);
+
+        inicializarComponentes();
+        iniciarFirebase();
+        firebaseAuth = FirebaseAuth.getInstance();
+        btnRegistrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String correo = etEmail.getText().toString();
+                String pass = etPass.getText().toString();
+                String passConf = etPassConf.getText().toString();
+                String clave = etClave.getText().toString();
+
+                if(correo.isEmpty() || pass.isEmpty() || passConf.isEmpty() || clave.isEmpty()){
+                    Toast.makeText(RegistroActivity.this, "No puede haber campos vacios", Toast.LENGTH_LONG).show();
+                }else if(!pass.equals(passConf)){
+                    etPassConf.requestFocus();
+                    etPass.requestFocus();
+                    etPassConf.setError("Las contraseñas no coinciden");
+                }else{
+                    firebaseAuth.createUserWithEmailAndPassword(correo, pass).addOnCompleteListener(RegistroActivity.this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if(task.isSuccessful()){
+                                Profesor profesor = new Profesor();
+                                profesor.setNombre(etNombre.getText().toString());
+                                profesor.setCorreo(etEmail.getText().toString());
+                                profesor.setPassword(etPass.getText().toString());
+                                profesor.setClave(etClave.getText().toString());
+                                databaseReference.child("Profesor").child(profesor.getClave()).setValue(profesor);
+                                startActivity(new Intent(RegistroActivity.this, DrawerMaestro.class));
+                            }else{
+
+                            }
+                        }
+                    });
+                }
+            }
+        });
+    }
+
+    private void inicializarComponentes(){
+
+        btnRegistrar = findViewById(R.id.btnRegistrarse);
+        etNombre = findViewById(R.id.etNomR);
+        etEmail = findViewById(R.id.etEmailR);
+        etPass = findViewById(R.id.etContR);
+        etPassConf = findViewById(R.id.etContConfR);
+        etClave = findViewById(R.id.etClaveR);
+
+    }
+
+    private void iniciarFirebase(){
+        FirebaseApp.initializeApp(this);
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        databaseReference = firebaseDatabase.getReference();
+
     }
 }
